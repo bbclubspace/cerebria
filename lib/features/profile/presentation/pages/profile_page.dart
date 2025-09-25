4010d6056c51a9319db7619cd8b7503eacc91052
Levent Aslan <lvnt.aslann@gmail.com>
2025-09-25 15:26:00 +0300

    feat: add earned badges card

--- lib/features/profile/presentation/pages/profile_page.dart
+++ lib/features/profile/presentation/pages/profile_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../../core/themes/app_colors.dart';
 import '../widgets/delete_account_card.dart';
+import '../widgets/earned_badges.dart';
 import '../widgets/profile_header.dart';
 import '../widgets/profile_info_card.dart';
 
@@ -11,13 +12,20 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final appColors = AppColors(isDarkMode: false);
-
+    final months = ["September", "October", "November"];
+    final badges = [
+      "assets/ranking-page-icon/first.png",
+      "assets/ranking-page-icon/second.png",
+      "assets/ranking-page-icon/third.png"
+    ];
     return Scaffold(
       backgroundColor: appColors.profile.pageBgColor,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           ProfileHeader(appColors: appColors),
+          SizedBox(height: 20.h),
+          EarnedBadges(appColors: appColors, months: months, badges: badges),
           SizedBox(height: 20.h),
           ProfileInfoCard(appColors: appColors),
           SizedBox(height: 20.h),
@@ -27,3 +35,4 @@ class ProfilePage extends StatelessWidget {
     );
   }
 }
+
